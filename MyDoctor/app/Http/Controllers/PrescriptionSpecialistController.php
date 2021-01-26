0d65fb3339b9b9b7836f56a82e4d9000a3be2195
Giuseppe Giacalone <giuseppe.giacalone@edu.unife.it>
2021-01-26 18:46:31 +0100

    model-controller-migrazioni vuote

---
+++ MyDoctor/app/Http/Controllers/PrescriptionSpecialistController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\prescription_specialist;
+use Illuminate\Http\Request;
+
+class PrescriptionSpecialistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\prescription_specialist  $prescription_specialist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(prescription_specialist $prescription_specialist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\prescription_specialist  $prescription_specialist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(prescription_specialist $prescription_specialist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\prescription_specialist  $prescription_specialist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, prescription_specialist $prescription_specialist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\prescription_specialist  $prescription_specialist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(prescription_specialist $prescription_specialist)
+    {
+        //
+    }
+}
---
+++ MyDoctor/app/prescription_medicine.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class prescription_medicine extends Model
+{
+    //
+}
