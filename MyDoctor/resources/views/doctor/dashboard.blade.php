9b9304f889360f4fec74b2cecfe2efc4fd5c9f14
rosaria <rosariaciociola1d@gmail.com>
2021-02-03 16:33:03 +0100

    card doctor

--- MyDoctor/resources/views/doctor/dashboard.blade.php
+++ MyDoctor/resources/views/doctor/dashboard.blade.php
@@ -1,40 +1,113 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="row">
                     <div class="card-header">Dashboard</div>
 
-                    <div class="card-deck">
-                        <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap" class="rounded">
-                            <div class="card-body">
-                            <h5 class="card-title">Appuntamenti</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                        <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap" class="rounded">
-                            <div class="card-body">
-                            <h5 class="card-title">Prescrivi ricetta farmaco</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                        <div class="card">
-                            <img class="card-img-top" src="..." alt="Card image cap" class="rounded">
-                            <div class="card-body">
-                            <h5 class="card-title">Prescrivi ricetta visita specialistica</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="row">
+                        <div class="col-12 col-lg-6">
+                            <!--start card-->
+                            <div class="shadow p-3 mb-5 bg-white">
+                                <div class="card-wrapper">
+                                    <div class="card card-img no-after">
+                                        <div class="img-responsive-wrapper">
+                                            <div class="img-responsive">
+                                                <figure class="img-wrapper">
+                                                    <img src="images/pexels-john-cahil-rom-3041851.jpg" title="img title" alt="imagealt" class="img-fluid">
+                                                </figure>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">I tuoi pazienti</h5>
+                                            <p class="card-text"></p><a class="read-more" href="#"><span class="text">Leggi di più</span>
+                                                <svg class="icon">
+                                                    <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-arrow-right"></use>
+                                                </svg></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--end card-->
                             </div>
                         </div>
+                        <div class="col-12 col-lg-6">
+                            <!--start card-->
+                            <div class="shadow p-3 mb-5 bg-white">
+                                <div class="card-wrapper">
+                                    <div class="card card-img no-after">
+                                        <div class="img-responsive-wrapper">
+                                            <div class="img-responsive img-responsive-panoramic">
+                                                <figure class="img-wrapper">
+                                                    <img src="images/pexels-suzy-hazelwood-3652103.jpg" title="img title" alt="imagealt" class="img-fluid">
+                                                </figure>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Ricette Farmaci</h5>
+                                            <p class="card-text"></p><a class="read-more" href="#"><span class="text">Leggi di più</span>
+                                                <svg class="icon">
+                                                    <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-arrow-right"></use>
+                                                </svg></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <!--start card-->
+                            <div class="shadow p-3 mb-5 bg-white">
+                                <div class="card-wrapper">
+                                    <div class="card card-img no-after">
+                                        <div class="img-responsive-wrapper">
+                                            <div class="img-responsive">
+                                                <figure class="img-wrapper">
+                                                    <img src="images/pexels-olya-kobruseva-5408689.jpg" title="img title" alt="imagealt" class="img-fluid">
+                                                </figure>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Appuntamenti</h5>
+                                            <p class="card-text"></p><a class="read-more" href="#"><span class="text">Leggi di più</span>
+                                                <svg class="icon">
+                                                    <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-arrow-right"></use>
+                                                </svg></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
+                        <div class="col-12 col-lg-6">
+                            <!--start card-->
+                            <div class="shadow p-3 mb-5 bg-white">
+                                <div class="card-wrapper">
+                                    <div class="card card-img no-after">
+                                        <div class="img-responsive-wrapper">
+                                            <div class="img-responsive img-responsive-panoramic">
+                                                <figure class="img-wrapper">
+                                                    <img src="images/pexels-gustavo-fring-3985166.jpg" title="img title" alt="imagealt" class="img-fluid">
+                                                </figure>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Ricette Visite Specialistiche</h5>
+                                            <p class="card-text"></p><a class="read-more" href="#"><span class="text">Leggi di più</span>
+                                                <svg class="icon">
+                                                    <use xlink:href="/bootstrap-italia/dist/svg/sprite.svg#it-arrow-right"></use>
+                                                </svg></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--end card-->
+                        </div>
                     </div>
-                </div>
+                
             </div>
         </div>
     </div>
+</div>
 @endsection
